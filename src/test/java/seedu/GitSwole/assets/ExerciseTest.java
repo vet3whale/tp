2d8c4d8a17eb238a7fa03b031ed4e1dfcfcd5d69
vet3whale <githubvetri@gmail.com>
2026-03-13 22:09:35 +0800

    Assets JUnit tests

---
+++ src/test/java/seedu/GitSwole/assets/ExerciseTest.java
@@ -0,0 +1,44 @@
+package seedu.GitSwole.assets;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DisplayName("Exercise")
+class ExerciseTest {
+
+    @Test
+    @DisplayName("Constructor sets all fields correctly")
+    void constructor_setsAllFields() {
+        Exercise e = new Exercise("bench press", 60, 4, 10);
+        assertEquals("bench press", e.getExerciseName());
+        assertEquals(60, e.getWeight());
+        assertEquals(4, e.getSets());
+        assertEquals(10, e.getReps());
+    }
+
+    @Test
+    @DisplayName("Setters update each field")
+    void setters_updateFields() {
+        Exercise e = new Exercise("squat", 0, 0, 0);
+        e.setExerciseName("deadlift");
+        e.setWeight(100);
+        e.setSets(5);
+        e.setReps(5);
+
+        assertEquals("deadlift", e.getExerciseName());
+        assertEquals(100, e.getWeight());
+        assertEquals(5, e.getSets());
+        assertEquals(5, e.getReps());
+    }
+
+    @Test
+    @DisplayName("Zero values are stored as-is")
+    void constructor_allowsZeroValues() {
+        Exercise e = new Exercise("plank", 0, 0, 0);
+        assertEquals(0, e.getWeight());
+        assertEquals(0, e.getSets());
+        assertEquals(0, e.getReps());
+    }
+}
